66ce58bd1c49df560d14d2999c6d000c05d9ec36
Kai Matsui <kai0927dentetsu@gmail.com>
2025-09-14 22:57:08 +0900

    bluetooth送信タイミングを修正

--- src/components/PowerControl.vue
+++ src/components/PowerControl.vue
@@ -4,7 +4,7 @@
       v-model="power"
       step="1"
       thumb-label
-      v-on:end="sendPower()"
+      @update:model-value="sendPower()"
     ></v-slider>
     <v-slider
       v-model="direction"
@@ -37,6 +37,9 @@ export default defineComponent({
     const direction = ref(1);
 
     function sendPower() {
+      console.log(
+        `Sending power: ${power.value} to control ID: ${props.controlId}`
+      );
       if (props.controlId !== undefined) {
         bluetoothStore.writePower(props.controlId, power.value);
       }
